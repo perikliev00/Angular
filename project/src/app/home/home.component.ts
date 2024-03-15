9f0aa24b88d9358e94ed0b24ee255fdc8e750df8
Aleksandar Perikliev <117957025+perikliev00@users.noreply.github.com>
2024-03-15 23:51:17 +0200

    Add files via upload

---
+++ project/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  a='age:23 soon 24...';
+  toggle=false;
+  handleClick() {
+    console.log('clicked');
+    this.toggle=!this.toggle;
+    console.log(this.toggle);
+  }
+}
